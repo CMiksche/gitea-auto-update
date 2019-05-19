cb4a36b9a21ce7f04df3f56b2704ceec71b9245e
Eideen <Torsteine@gmail.com>
2019-05-19 12:47:20 +0200

    Merge update.py

--- updater.py
+++ updater.py
@@ -9,35 +9,10 @@ License: GNU General Public License
 import settings
 import requests
 import os
-
-
-# Function to download a file
-def download(url, file_name):
-    # open in binary mode
-    with open(file_name, "wb") as file:
-        # get request
-        response = requests.get(url)
-        # write to file
-        print ("writing file", file_name)
-        file.write(response.content)
-
-# Function to build the new version from source
-def buildFromSource(tag):
-    # Change to source dir
-    os.chdir(settings.source_dir)
-    # Checkout master
-    os.system("git checkout master")
-    # Update
-    os.system("git pull")
-    # Checkout relase branch
-    os.system("git checkout "+tag)
-    # Build from source
-    os.system('TAGS="bindata sqlite sqlite_unlock_notify" make generate build')
-    # Move binary
-    os.system("mv gitea "+settings.gtfile)
+import functions
 
 def is_tool(name):
-    ##Check whether `name` is on PATH and marked as executable. 
+    ##Check whether `name` is on PATH and marked as executable.
 
     # from whichcraft import which
     from shutil import which
