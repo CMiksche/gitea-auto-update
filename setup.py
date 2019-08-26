56f128b2f7c2ce36959163d4085c9eb8d2729d4c
Christoph Miksche <christoph@miksche.org>
2019-08-26 21:33:28 +0200

    feat: add long description

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
-from setuptools import setup
+import setuptools
 
-setup(name='gitea_auto_update',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='gitea_auto_update',
       version='2.0.0',
       description='A script which can update gitea to a new version.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/CMiksche/gitea-auto-update',
       author='Christoph Miksche',
       author_email='christoph@miksche.org',
       license='GPLv3',
       classifiers=[
+            "Programming Language :: Python :: 3",
             "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
             "Operating System :: Unix",
       ],
@@ -17,7 +23,7 @@ setup(name='gitea_auto_update',
             'packaging',
             'fire'
       ],
-      packages=['update'],
+      packages=setuptools.find_packages(),
       entry_points={
           'console_scripts': ['gitea-auto-update=update.__init__:main'],
       }
